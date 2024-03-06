f1a3e31b266292dce74ff6b1ff2bcadf06ac2c37
Hung <trannguyenquochung00@gmail.com>
2024-03-06 14:23:52 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Hưng Trần on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
